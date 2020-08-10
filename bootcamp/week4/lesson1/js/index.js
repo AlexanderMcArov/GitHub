7b8513fa4ba58cfd3ff3b0a4c281f45e0d5e11c4
AlexanderMcArovHP <alexandercom445@gmail.com>
2020-08-10 16:27:36 +0600

    week4 lesson1 HP

--- bootcamp/week4/lesson1/js/index.js
+++ bootcamp/week4/lesson1/js/index.js
@@ -1,168 +1,180 @@
-// // // // console.log('Hello');
+// // // console.log('Hello');
 
-// // // let example = 1;
-// // // let exstring = "String Example";
-// // // let example2 = 'Example2';
-// // // let a = 5;
-// // // let b = 20;
-// // // let c = a + b;
-// // // let camelCase = 'D';
-// // // let hello_world = 'Hellow world'
-// // // let set1 = 1;
-// // // let $hello = "Hello Dollar"
-// // // let _hello = 'Hello Down border';
+// // let example = 1;
+// // let exstring = "String Example";
+// // let example2 = 'Example2';
+// // let a = 5;
+// // let b = 20;
+// // let c = a + b;
+// // let camelCase = 'D';
+// // let hello_world = 'Hellow world'
+// // let set1 = 1;
+// // let $hello = "Hello Dollar"
+// // let _hello = 'Hello Down border';
 
-// // const conts = 1;
-// // console.log(conts)
-
-
-// // // Типы данных
+// const conts = 1;
+// console.log(conts)
 
 
-// // // String
+// // Типы данных
 
-// // let string = 'String';
-// // let string2 = `String\n\tDown\n\t\tDown`;
-// // console.log(string2);
 
-// // // Number
+// // String
 
-// // let number = 123;
-// // let number2 = 123.25;
-// // console.log(number+number2);
-// // /*
-// // multiComment
-// // */
+// let string = 'String';
+// let string2 = `String\n\tDown\n\t\tDown`;
+// console.log(string2);
 
-// // // Boolean
+// // Number
 
-// // let booleanT = true;
-// // let booleanF = false;
-// // console.log(booleanF)
+// let number = 123;
+// let number2 = 123.25;
+// console.log(number+number2);
+// /*
+// multiComment
+// */
 
-// // // n4 Undefined
+// // Boolean
 
-// // let a = undefined;
-// // console.log(a + 5);
+// let booleanT = true;
+// let booleanF = false;
+// console.log(booleanF)
 
-// // // NULL
+// // n4 Undefined
 
-// // let b = null;
-// // console.log(b);
+// let a = undefined;
+// console.log(a + 5);
 
-// // // object
+// // NULL
 
-// // let object = {
-// //     name: 'Sanya',
-// //     age: 24,
-// //     email: 'alexandercom445@gmail.com',
-// //     hobby: {
-// //         key: 2
-// //     },
-// //     dog: {
-// //         name: 'Sharik',
-// //         color: {
-// //             fon: ['22',33,44,{
-// //                 r:1,
-// //                 g:2
-// //             }],
-// //             main: 10
-// //         },
-// //         age: 20
-// //     }
-// // };
+// let b = null;
+// console.log(b);
 
-// // let x = 'name';
-// // console.log(object.name);
-// // console.log(object);
-// // console.log(object.dog.color.fon[object.dog.color.fon.length-1].g);
+// // object
 
-// // // Arrow
+// let object = {
+//     name: 'Sanya',
+//     age: 24,
+//     email: 'alexandercom445@gmail.com',
+//     hobby: {
+//         key: 2
+//     },
+//     dog: {
+//         name: 'Sharik',
+//         color: {
+//             fon: ['22',33,44,{
+//                 r:1,
+//                 g:2
+//             }],
+//             main: 10
+//         },
+//         age: 20
+//     }
+// };
 
-// // let arrow = [
-// //     {
-// //         name: 'ArrowUser'
-// //     },1,2,3,4,5,6,7,8,9,10
-// // ]
+// let x = 'name';
+// console.log(object.name);
+// console.log(object);
+// console.log(object.dog.color.fon[object.dog.color.fon.length-1].g);
 
-// // console.log(arrow.length);
-// // console.log(arrow[0].name);
-// // console.log(arrow[(arrow.length-1)]);
-// // console.log(arrow[9]); 
+// // Arrow
 
-// // let arrlist = []
+// let arrow = [
+//     {
+//         name: 'ArrowUser'
+//     },1,2,3,4,5,6,7,8,9,10
+// ]
 
-// // arrlist[0] = 1;
-// // arrlist[1] = 2;
+// console.log(arrow.length);
+// console.log(arrow[0].name);
+// console.log(arrow[(arrow.length-1)]);
+// console.log(arrow[9]); 
 
-// // console.log(arrlist[1])
+// let arrlist = []
 
-// // // математические операторы
-// // console.log('Математические операторы')
-// // console.log('Вводные: 5 и 5')
-// // console.log('+ '+(5+5))
-// // console.log('- '+(5-5))
-// // console.log('* '+(5*5))
-// // console.log('/ '+(5/5))
-// // console.log('% '+(6%5))
-// // console.log('10-10a '+(10 - '10a'))
+// arrlist[0] = 1;
+// arrlist[1] = 2;
 
-// // a = 10;
-// // console.log(a++)
-// // console.log(a)
+// console.log(arrlist[1])
 
-// // console.log(' typeof '+typeof(a))
-// // console.log(' typeof '+typeof(NaN))
-// // console.log(' typeof Infinity '+typeof(Infinity))
-// // console.log(' isNaN ' + isNaN(10))
+// // математические операторы
+// console.log('Математические операторы')
+// console.log('Вводные: 5 и 5')
+// console.log('+ '+(5+5))
+// console.log('- '+(5-5))
+// console.log('* '+(5*5))
+// console.log('/ '+(5/5))
+// console.log('% '+(6%5))
+// console.log('10-10a '+(10 - '10a'))
 
-// // console.log(parseInt(15))
-// // console.log(parseInt('123dsadsa'))
-// // console.log(typeof(parseInt('123dsadsa')))
-// // console.log(parseFloat('123.55dsadsa'))
+// a = 10;
+// console.log(a++)
+// console.log(a)
 
-// // console.log( 5 > 4 )
+// console.log(' typeof '+typeof(a))
+// console.log(' typeof '+typeof(NaN))
+// console.log(' typeof Infinity '+typeof(Infinity))
+// console.log(' isNaN ' + isNaN(10))
 
-// // // Нестрогое сравнение
+// console.log(parseInt(15))
+// console.log(parseInt('123dsadsa'))
+// console.log(typeof(parseInt('123dsadsa')))
+// console.log(parseFloat('123.55dsadsa'))
 
-// // console.log(10 == 10)
-// // console.log(10 == '10')
+// console.log( 5 > 4 )
 
-// // // строгое сравние
+// // Нестрогое сравнение
 
-// // console.log(10 === 10)
-// // console.log(10 === '10')
+// console.log(10 == 10)
+// console.log(10 == '10')
 
-// // Логические операторы
+// // строгое сравние
 
-// // console.log(true && false)
-// // console.log(false || false)
-// // console.log(false || false)
-// // console.log(!false)
+// console.log(10 === 10)
+// console.log(10 === '10')
+
+// Логические операторы
 
 // console.log(true && false)
-// console.log(true || false)
+// console.log(false || false)
+// console.log(false || false)
+// console.log(!false)
 
-// {
-//     let str = "string Local"
-//     alert(str)
-//     let a,b
-//     a = 10
-//     b = a
-//     console.log(b)
-// }
-// // let age = confirm('You age > 18?')
-// // console.log(age)
-// // let pro = prompt("You name?")
-// // console.log(pro)
+console.log(true && false)
+console.log(true || false)
+console.log(5 > 10)
 
-// let ohh = {
-//     name: 'Sanya'
-// }
 
-// let json = JSON.stringify(ohh)
-// console.log(json)
-// console.log(ohh.name)
+let age = 21;
+let sport = 4;
 
-// let jsonpar = JSON.parse(json)
-// console.log(jsonpar)
+
+{
+    let str = "string Local"
+    let a,b
+    a = 10
+    b = a
+    console.log(b)
+}
+// let age = confirm('You age > 18?')
+// console.log(age)
+// let pro = prompt("You name?")
+// console.log(pro)
+
+let ohh = {
+    name: 'Sanya'
+}
+
+let json = JSON.stringify(ohh)
+console.log(json)
+console.log(ohh.name)
+
+let jsonpar = JSON.parse(json)
+console.log(jsonpar)
+
+let block = document.getElementsByClassName('block')
+// document.getElementById("block").innerHTML = JSON.stringify(block);
+document.getElementById('btn_download').innerHTML = 'Button'
+console.log(block)
+console.log(block.textContent)
+console.log(document.body.innerHTML)
