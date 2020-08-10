92acdba26769688894c7d68aace7a41db967a524
AlexanderMcArovHP <alexandercom445@gmail.com>
2020-08-10 10:30:15 +0600

    Change Bootcamp

---
+++ bootcamp/week4/lesson1/js/index.js
@@ -0,0 +1,51 @@
+// // console.log('Hello');
+
+// let example = 1;
+// let exstring = "String Example";
+// let example2 = 'Example2';
+// let a = 5;
+// let b = 20;
+// let c = a + b;
+// let camelCase = 'D';
+// let hello_world = 'Hellow world'
+// let set1 = 1;
+// let $hello = "Hello Dollar"
+// let _hello = 'Hello Down border';
+
+const conts = 1;
+console.log(conts)
+
+
+// Типы данных
+
+
+// String
+
+let string = 'String';
+let string2 = `String\n\tDown\n\t\tDown`;
+console.log(string2);
+
+// Number
+
+let number = 123;
+let number2 = 123.25;
+console.log(number+number2);
+/*
+multiComment
+*/
+
+// Boolean
+
+let booleanT = true;
+let booleanF = false;
+console.log(booleanF)
+
+// n4 Undefined
+
+let a = undefined;
+console.log(a + 5);
+
+// NULL
+
+let b = null;
+console.log(b);
