384a213f4dfd844537fbf0f9f0093e25bf80034e
AlexanderMcArovHP <alexandercom445@gmail.com>
2020-08-26 20:04:11 +0600

    week6 Lesson3

---
+++ bootcamp/chaptor6/lesson3/main.js
@@ -0,0 +1,208 @@
+// // // console.log('Week6 Lesson3');
+
+// // // let list = [
+// // //     'Соль',
+// // //     'Фасоль',
+// // //     'Масло',
+// // //     'Гречка',
+// // //     'Сахар'
+// // // ]
+
+
+// // // let [first,second,...other] = list
+
+// // // console.log(first);
+
+// // // let arr = [
+// // //     [0,1,2,3,4],
+// // //     [5,6,7,8,9]
+// // // ]
+
+// // // let [[a,b,c,d,e],arr2] = arr
+
+// // // console.log(d);
+
+
+// // // let person = {
+// // //     name: 'Sanya',
+// // //     age: 24,
+// // //     job: 'IT',
+// // //     skills:{
+// // //         js: 40,
+// // //         html: 50,
+// // //         css: 70
+// // //     }
+// // // }
+
+// // // let {name,job,age} = person
+// // // let {skills:{js}} = person
+
+// // // console.log(name,job,age);
+// // // console.log(js);
+
+// // // let obj2 = {
+// // //     arr:[
+// // //         {
+// // //             name3:'Sanya'
+// // //         }
+// // //     ]
+// // // }
+
+// // // let {arr:[{ name3 }]} = obj2
+// // // console.log(name);
+
+// // // function foo({nameStambek}) {
+// // //     console.log(nameStambek);
+// // // }
+
+// // // let obj5 = {
+// // //     nameStambek: 'Stambek'
+// // // }
+
+// // // foo(obj5)
+
+
+// // // let objk = [0,1,2,3,4,5,6]
+// // // let [,,,newArr] = objk
+// // // console.log(newArr);
+// // // // console.log(...objk);
+
+// // // function bar(a,b,c,d,e,f,g,h){
+// // //     console.log(a,b,c,d,e,f,g,h);
+// // // }
+
+// // // bar(...objk)
+
+
+// // // function sortMe(a,b) {
+// // //     let result = a - b
+// // //     if(result < 0) return a 
+// // //     else return b
+// // // }
+// // // console.log(sortMe(6,5));
+
+
+// // // let object1 = {
+// // //     name: 'Sanya'
+// // // }
+// // // let object2 = {...object1}
+// // // console.log(object1.name);
+
+// // // // object2.name = 'Aktan'
+
+// // // console.log(object1.name);
+// // // console.log(object2.name);
+
+// // // let boj22 = {
+// // //     Name: 'Kubic',
+// // //     color: 'black',
+// // //     xyz: [50,60,70]
+// // // }
+
+// // // let boj32 = {
+// // //     LastName: 'Rubik'
+// // // }
+
+// // // let boj42 = {...boj22, ...boj32}
+// // // console.log(boj42);
+
+
+// // // // let as = {}
+// // // // let bs = as
+// // // // as.bs = bs
+// // // // console.log(bs);
+
+
+// let obiect = {
+//     name:'Sanya',
+//     age: 22,
+//     position:[
+//         'mentor',
+//         'developer',
+//         'team lead',
+//         'project manager'
+//     ],
+//     dog:{
+//         name: 'Laika',
+//         age: 2
+//     }
+// }
+
+// // let {
+// //     name,
+// //     age,
+// //     // dog:{
+// //     //     name: dogName
+// //     // },
+// //     positions:[
+// //         firstPosition,
+// //         secondPosition,
+// //         ...otherPositions
+// //     ]
+// // } = obiect
+
+// // console.log(name,positions);
+
+// function sayHello({name,age,position:[first]}) {
+//     console.log(
+//         `Hello,i'm ${name},
+//         I'm ${age} y.o,
+//         My dog ${first}`
+//     );
+// }
+
+// sayHello(obiect)
+
+
+// console.log();
+
+// let {log:print} = console
+
+// print('Lol')
+
+// print(console)
+
+
+// console.timeStamp('dsa')
+
+// console.warn('INFO')
+
+
+// let actan = {
+//     name: 'Aktan',
+//     age: 20
+// }
+
+// let c = actan
+// c.name = 'Kubat'
+
+// console.log(c);
+// console.log(name);
+
+let array = [1,2,3]
+let [a,b,c] = array
+
+function func(arr) {
+    return a
+}
+
+console.log(a);
+
+console.log(window);
+
+
+let arr = ['Aktan','Kubat','Aleksandr']
+
+function destruct(arr,i=0) {
+    if(i === arr.length) return
+    if(Array.isArray(arr[i])) destruct(arr[i])
+    else window[`${arr[i]}`] = arr[i]
+    destruct(arr, i + 1)
+    return window[`${arr[i]}`]
+}
+
+destruct(arr)
+window.avto = function auto(a) {
+    return a
+}
+console.log(Aktan,Kubat,Aleksandr);
---
+++ bootcamp/chaptor6/lesson3/task/main.js
@@ -0,0 +1,47 @@
+console.log('Week 6 Lesson 3')
+
+
+// Task 1
+// у вас есть масив:
+// let arr = ["Julius", "Caesar", "Consul", "of the Roman Republic"]
+// Вам надо поместить в переменную firstName 1-элемент
+// в переменную titul  3-элемент 
+let arr1 = ["Julius", "Caesar", "Consul", "of the Roman Republic"]
+let [firstName,,titul] = arr1
+console.log('Task 1',firstName,titul);
+
+// // Task 2
+// // У вас есть строка `Faisal Iraqi`
+// // поместите в переменную firstName первое слово
+// // в переменную lastName второе слово
+let string2 = `Faisal Iraqi`
+let arr2 = string2.split(' ')
+let [firstName2,lasName2] = arr2
+console.log('Task 2',firstName2,lasName2);
+
+// Task 3
+// создайте объект 
+// let obj = {};
+// в свойство firstName первое слово
+// в свойство lastName второе слово
+
+let obj = {
+    firstName: firstName,
+    titul: titul
+}
+console.log('Task 3',obj.firstName,obj.titul);
+
+// Task 4
+// Создайте переменную let a = 0  и let b = 1
+// поменяйте их значения местами используя деструктуризацию массива 
+
+let a = 0
+let b = 1
+let arr = [a,b]
+let [c,d] = arr
+a = d
+b = c
+console.log('Task 4',a,b);
+
+
+
