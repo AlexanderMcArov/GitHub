6d335ababe90f4552fbd2ce825b72052d568e342
Alexander McArov <alexandercom445@gmail.com>
2020-09-03 20:33:14 +0300

    Update

--- bootcamp/chaptor7/Lesson4/task/main.js
+++ bootcamp/chaptor7/Lesson4/task/main.js
@@ -59,6 +59,100 @@ function moveRight(){
 // Создайте модальное окно по аналогии с компонентом Bootstrap:
 
 let modalWindow = document.querySelector('div.modalWindow')
+let modalBox = document.querySelector('div.modal_box')
+let btn_modal_window = document.querySelector('#btn_modal_window')
+let btn_close = document.querySelector('#btn_close')
+let btn_sub = document.querySelector('#btn_sub')
+let wclose = true
+
 console.log(modalWindow)
 modalWindow.style.display = 'none'
 
+btn_modal_window.addEventListener('click',()=>{
+    modalWindow.style.display = 'block'
+    modalBox.style.animation = 'modal 0.5s  linear 0s 1 normal forwards'
+})
+modalBox.addEventListener('mouseenter',()=>{
+    console.log('MouseEnter');
+    wclose = false
+})
+modalBox.addEventListener('mouseleave',()=>{
+    console.log('MouseLeave');
+    wclose = true
+    
+})
+document.addEventListener('mouseup',()=>{
+    if(wclose){
+        modalWindow.style.display = 'none'
+    }
+})
+btn_close.addEventListener('click',()=>{
+    modalWindow.style.display = 'none'
+})
+btn_sub.addEventListener('click',()=>{
+    let save = document.querySelector('.modal_sub')
+    save.innerHTML = 'Modal Access'
+    modalWindow.style.display = 'none'
+})
+
+// После просмотра последнего видео урока, сделать контактную книжку с использованием
+// localStorage. На подобии туду листа. Но уже с 3 инпутами(Ф, И и номер телефона)
+
+let localBook = document.querySelector('div.localBook')
+let book_name = document.querySelector('#book_name')
+let book_number = document.querySelector('#book_number')
+let btn_book = document.querySelector('#btn_book')
+let book_content = document.querySelector('div.book_content')
+let list = ['','']
+let update_book = true
+book_name.addEventListener('change',()=>{
+    console.log(book_name.value);
+    list[0] = book_name.value
+})
+book_number.addEventListener('change',()=>{
+    console.log(book_number.value);
+    list[1] = book_number.value
+})
+btn_book.addEventListener('click',()=>{
+    if(!list[0] || !list[1]) return false
+    // let newElem = document.createElement('div')
+    // newElem.classList.add('book_item')
+    // newElem.innerText = `Name: ${list[0]} || Number: ${list[1]}`
+    // book_content.appendChild(newElem)
+    localStorage.setItem(list[0],list[1])
+    update_book = true
+    updatebook()
+    list[0] = ''
+    list[1] = ''
+    book_name.value = ''
+    book_number.value = ''   
+})
+
+function updatebook() {       
+    if(update_book){
+        book_content.innerText = '' 
+        let keys = Object.keys(localStorage);
+        for(let key of keys) {
+        let newElem = document.createElement('div')
+        newElem.classList.add('book_item')
+        newElem.innerText = `Name: ${key} || Number: ${localStorage[key]}`
+        book_content.appendChild(newElem)
+        }
+    } 
+    update_book = false
+}
+
+function clearbook() {
+    localStorage.clear()
+    book_content.innerText = ''
+    update_book = true
+}
+
+let drag = document.querySelectorAll('div.drag')
+console.log(drag);
+drag = Object.keys(drag);
+for(let key in drag){
+    window[drag[key]] = drag[key].addEventListener('mouseenter',()=>{
+        console.log(key);
+    })
+}
