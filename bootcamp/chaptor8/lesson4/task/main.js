f612f9148132a1629e3662b4e69c834b3f338d03
Alexander McArov <alexandercom445@gmail.com>
2020-09-10 19:46:50 +0300

    Home

---
+++ bootcamp/chaptor8/lesson4/task/main.js
@@ -0,0 +1,51 @@
+console.log('Tamagocha');
+
+let buttons = document.querySelectorAll('button')
+let gameTitle = document.querySelector('.main_title')
+let info = document.querySelectorAll('.foo_info')
+
+class Tamagocha{
+    constructor(name,hungry,happines){
+        this.name = name
+        this.hungry = hungry
+        this.happines = happines
+    }
+    say(){
+        this.hungry--
+        this.happines--
+        return `Hello. I'm ${this.name}!`
+    }
+    play(){
+        if(this.hungry < 40) return `${this.name} doesn't want to play.`
+        this.hungry--
+        this.happines++
+        info[1].innerHTML = "Hungry: " + this.hungry
+        info[2].innerHTML = "Happy: " + this.happines
+        return `${this.name} Playing: Hungry => ${this.hungry}, Happy => ${this.happines}`
+    }
+    eat(){
+        if(this.eat == 100) return `${this.name} doesn't want to eat.`
+        this.hungry = this.hungry + 2
+        this.happines--
+        info[1].innerHTML = "Hungry: " + this.hungry
+        info[2].innerHTML = "Happy: " + this.happines
+        return `${this.name} Playing: Hungry => ${this.hungry}, Happy => ${this.happines}`
+    }
+}
+
+function main(){
+    let Catty = new Tamagocha('Catty',60,80)
+    info[0].innerHTML = "Name: " + Catty.name
+    buttons.forEach(item => {
+        item.addEventListener('click',function(){
+            console.log(item.getAttribute('id'));
+            if(item.getAttribute('id') == 'play'){
+                gameTitle.innerHTML += "<br>" + Catty.play()
+            }else if(item.getAttribute('id') == 'eat'){
+                gameTitle.innerHTML += "<br>" + Catty.eat()
+            }
+        })
+    })
+}
+
+main()
